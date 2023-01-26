849ba0c5b4e1762d5ff7cf266555ef42013a4e7d
Ganil Batist <13810327+Ganil151@users.noreply.github.com>
2023-01-26 09:06:43 -0500

    register/user

--- myapp/views.py
+++ myapp/views.py
@@ -1,7 +1,8 @@
-from pyexpat import features
-from unicodedata import name
-from django.shortcuts import render
+from email import message
+from django.contrib import messages
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.models import User, auth
 from .models import Feature
 
 # Create your views here.
@@ -12,7 +13,35 @@ def index(request):
     return render(request, 'index.html', {'features': features})
 
 
+def register(request):
+    if request.method == 'POST':
+        username = request.POST['username'] 
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+
+        if password == password2:
+            if User.objects.filter(email=email).exists():
+                messages.info(request, 'Email Already Used')
+                return redirect('register')
+            elif User.objects.filter(username=username).exists():
+                messages.info(request, 'Username Already Used')
+                return redirect('register')
+            else:
+                user = User.objects.create_user(
+                    username=username, email=email, password=password)
+                user.save()
+                return redirect('login')
+        else:
+            messages.info(request, 'Password Not The Same')
+    else:
+        return render(request, 'register.html')
+
+
 def counter(request):
     text = request.GET['text']
     amount_of_words = len(text.split())
     return render(request, 'counter.html', {'amount': amount_of_words})
+
+
+# Lesson Stopped at: 6:08:49, from: https://youtu.be/jBzwzrDvZ18?t=22129
